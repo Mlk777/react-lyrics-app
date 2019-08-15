56b62e439106a78797cc539404e9abfe491d852b
Mlk777 <>
2019-08-15 15:48:43 +1200

    Updates

---
+++ precache-manifest.74e5d2b19e1685067567e8231fcc9f99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2edfdfd680cb88895b3ac9cf529bfe",
+    "url": "/react-lyrics-app/index.html"
+  },
+  {
+    "revision": "2a8076bb09849afa74c5",
+    "url": "/react-lyrics-app/static/js/2.dedbab28.chunk.js"
+  },
+  {
+    "revision": "75a1b17d49b1d11c16e1",
+    "url": "/react-lyrics-app/static/js/main.267333a2.chunk.js"
+  },
+  {
+    "revision": "64f4a2edff60c541e441",
+    "url": "/react-lyrics-app/static/js/runtime~main.caa7a38d.js"
+  }
+]);
